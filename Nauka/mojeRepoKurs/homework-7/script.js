10b1909cf6f6dc3c88dde9b4d3347f4e16f112a7
Arkadiusz Dudziński <Moria476@gmail.com>
2023-01-03 04:39:58 +0100

    zadanie7

--- Nauka/mojeRepoKurs/homework-7/script.js
+++ Nauka/mojeRepoKurs/homework-7/script.js
@@ -1,41 +1,53 @@
 async function fetchData(name) {
   const response = await fetch(`https://api.github.com/users/${name}`);
   const user = await response.json();
-  //console.log(user);
-  showProfile(user, name);
+  const responseRepos = await fetch(
+    `https://api.github.com/users/${name}/repos`
+  );
+  const repos = await responseRepos.json();
+  showProfile(user, repos);
 }
+const profileLinks = document.querySelector(".main__div__right__links");
 
-const showProfile = async (user, name) => {
+const showProfile = async (user, repos) => {
   const profileImage = document.querySelector("#profileImage");
   const profileName = document.querySelector("#profileName");
   const profileDescription = document.querySelector("#description");
   const profileFollowers = document.querySelector("#numberFollowers");
   const profileFollowing = document.querySelector("#numberFollowing");
   const profileRepos = document.querySelector("#numberRepos");
-  const profileLinks = document.querySelector(".main__div__right__links");
-  const response = await fetch(`https://api.github.com/users/${name}/repos`);
-  const repos = await response.json();
-
-  for (i = 0, j = 0; i < repos.length && j < 10; i++, j++) {
-    console.log(repos[i]);
+  for (i = 0; i < repos.length && i < 10; i++) {
     const link = document.createElement("a");
     link.href = `${repos[i].html_url}`;
-    console.log(repos[i].html_url);
     link.innerHTML = repos[i].name;
-    console.log(repos[i].name);
     profileLinks.append(link);
   }
-
   profileImage.src = user.avatar_url;
   profileName.innerHTML = user.name;
-  if (user.bio === null) {
-    profileDescription.innerHTML = "Description not provided";
-  } else {
+  if (!user.bio === null) {
     profileDescription.innerHTML = user.bio;
   }
   profileFollowers.innerHTML = user.followers;
   profileFollowing.innerHTML = user.following;
   profileRepos.innerHTML = user.public_repos;
 };
-
-fetchData("gaearon");
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+const searchInput = document.querySelector("#search");
+searchInput.addEventListener("change", (event) => {
+  event.preventDefault();
+  removeAllChildNodes(profileLinks);
+  fetchData(event.target.value);
+});
+const form = document.querySelector("#form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (event.key === "Enter") {
+    removeAllChildNodes(profileLinks);
+    fetchData(event.target.value);
+  }
+  searchInput.blur();
+});

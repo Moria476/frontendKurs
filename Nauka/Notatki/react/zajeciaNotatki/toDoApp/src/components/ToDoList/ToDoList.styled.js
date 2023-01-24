b2a41895b6f2b28d17663f9c8865647b983471f3
Moria476 <Moria476@gmail.com>
2023-01-24 07:29:30 +0100

    zadanie Jakub skonczone

--- Nauka/Notatki/react/zajeciaNotatki/toDoApp/src/components/ToDoList/ToDoList.styled.js
+++ Nauka/Notatki/react/zajeciaNotatki/toDoApp/src/components/ToDoList/ToDoList.styled.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
-export const ToDoList = styled.section`
-  min-height: 400px;
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  border-radius: 5px;
-  padding: 10px;
-  width: 400px;
-  background-color: magenta;
-  border-width: 5px;
-  border-color: black;
-  border-style: solid;
+export const ToDoListStyle = styled.section`
+	min-height: 400px;
+	display: flex;
+	flex-direction: column;
+	gap: 5px;
+	border-radius: 5px;
+	padding: 10px;
+	width: 400px;
+	background-color: magenta;
+	border-width: 5px;
+	border-color: black;
+	border-style: solid;
 `;
